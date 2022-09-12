524bf1e7058db83abd19621a482507172fb61157
kusumakter959 <kusumaker.dev@gmail.com>
2022-09-12 18:55:48 +0600

    JavaScript Template Literal

---
+++ js/templateLiterals.js
@@ -0,0 +1,13 @@
+
+let a  = `It's my Life`;
+let b  = `It's my Life
+            my rules`;
+console.log(b);
+
+
+//Interpolation
+
+
+let country = "bangladesh";
+let santace = `I love  ${country} `
+console.log(santace);
