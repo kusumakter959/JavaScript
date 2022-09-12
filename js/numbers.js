e75be71b0e64edade0e025ac02ef7c35347e680a
kusumakter959 <kusumaker.dev@gmail.com>
2022-09-12 19:39:26 +0600

    JavaScript Numbers

---
+++ js/numbers.js
@@ -0,0 +1,26 @@
+//Nmbers always floting numbers
+let x = 123e5;
+let y = 123e-5;
+
+let a = 999999999999999; //meximum 15
+let b = 99999999999999999; //beshi dile accurent na hote pare
+
+let c = 0.2 + 0.1; ///error
+let d = (0.2 * 10 + 0.1 * 10) / 10; //slove
+
+let x1 = "10";
+let x2 = 2;
+console.log(x1 + x2); //jokhon pluse hoy, tokhon concetenate kore ney
+
+let m = "10";
+let n = 2;
+
+console.log(m / n);
+
+//NAN not a Number
+
+//number check korar jonno
+let i = "10e5";
+let k = 10;
+console.log(isNaN(i));
+console.log(isNaN(k));
