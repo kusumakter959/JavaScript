64f4bae361bcac915c98929049a66985d5aaef4f
kusumakter959 <kusumaker.dev@gmail.com>
2022-09-12 12:30:34 +0600

    JavaScript slice/substring/substr methord

--- js/stringMethod.js
+++ js/stringMethod.js
@@ -5,3 +5,22 @@ const a = {
 }
  const s = "Bangladesh";
  console.log(s.length);
+
+// Slice
+let str = "Apple, Banna, Kiwi";
+let banana= str.slice(7); //kono ekta variable theke data khete newa hoy
+let bannana= str.slice(7, 13); //kono ekta variable theke data khete newa hoy
+console.log(bannana);
+
+// Sbustr sob somoy positive theke start korte hoy,, negative accept kore na
+
+let sub = "Apple, Banna, Kiwi";
+let substring= str.substring(0, 13); //kono ekta variable theke data khete newa hoy
+console.log(substring);
+
+
+//Sbustr 1st kotutoko nibe,, erpor kotu gor pojjonto nibe
+
+let substr1 =  "Apple, Banna, Kiwi";
+let substr2= substr1.substr(7, 5); //kono ekta variable theke data khete newa hoy
+console.log(substr2);
