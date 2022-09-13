3ebed5c87e5c8ed6dcec4c3c7f5883dcc24ea39e
kusumakter959 <kusumaker.dev@gmail.com>
2022-09-13 14:36:16 +0600

    JavaScript Date Formate

---
+++ date.js
@@ -0,0 +1,18 @@
+
+
+const date = new Date();
+const date1 = new Date(2022,9,13,12,20,34,0);
+
+// ISO FORMATE
+const date11 = new Date("2022-03-23"); //y-m-d
+const date2 = new Date("2022-03");
+
+// SHORT DATE fromate
+// const date3 = new Date("03/23/2022"); //m/d/y
+const d = new Date("03/25/2015");
+
+
+// SHORT DATE formate
+const d1 = new Date("Mar 25 2015");
+
+console.log(d1);
