ef970399b39cc93ca708dbb9abe4b8e0c15d2043
kusumakter959 <kusumaker.dev@gmail.com>
2022-09-13 16:22:14 +0600

    JavaScript  Random

--- js/math.js
+++ js/math.js
@@ -1,25 +1,25 @@
 console.log(Math.SQRT1_2);
 
 //Round hocche dosomike na rekhe integere niye jai
-// console.log(Math.round(4.6));
+console.log(Math.round(4.6));
 
 //Math.Ceil dosomiker sathe kichu thakle setha 1 add kore
-// console.log(Math.ceil(4.2));
+console.log(Math.ceil(4.2));
 
 //Math.floor niche dike gele 1 add kore integer kore uporer dike gele dosomik k hisab kore na
-//console.log(Math.floor(-4.3));
+console.log(Math.floor(-4.3));
 
 //Math.trunc dosomiker por ja theke, se gulu khete fele dey..
 console.log(Math.trunc(4.7));
 
 // Math.sign always positive hole 1 return KeyboardEvent, negative hole -1, and 0 hole 0 retun kore
 
-// console.log(Math.sign(1));
-// console.log(Math.sign(0));
+console.log(Math.sign(1));
+console.log(Math.sign(0));
 
 // Math.pow power dile kotu hobe setha ber Kore
-// console.log(Math.pow(8, 2)); //1st kake power korbo, 2nd kotu powar dibo
-// console.log(Math.pow(100, 2));
+console.log(Math.pow(8, 2)); //1st kake power korbo, 2nd kotu powar dibo
+console.log(Math.pow(100, 2));
 
 //Math.abs jekono nagative value k amra abs korle setha positive hoye jai
 
---
+++ js/random.js
@@ -0,0 +1,5 @@
+
+console.log(Math.random());
+// console.log(Math.random() * 10);
+// console.log(Math.random() * 11);
+console.log(Math.floor(Math.random() * 11)+1);
