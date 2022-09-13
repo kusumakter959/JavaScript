98f47f0a8df6a6bbd22ef1a023a5b8d60fee48aa
kusumakter959 <kusumaker.dev@gmail.com>
2022-09-13 09:01:59 +0600

    JavaScript method

---
+++ js/number_properties.js
@@ -0,0 +1,11 @@
+// Max Values
+ let max = Number.MAX_VALUE;
+ let min = Number.MIN_VALUE;
+
+    let x = 1/0;
+let inifinit = Number.POSITIVE_INFINITY;
+    let y = -1/0;
+let inifinitN = Number.NEGATIVE_INFINITY;
+console.log(y);
+
+ 
