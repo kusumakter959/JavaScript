f650783e2933332bc1b5e72b3302fe76062d8a51
kusumakter959 <kusumaker.dev@gmail.com>
2022-09-13 14:49:35 +0600

    JavaScript Get Date Methods

---
+++ js/dateMethods.js
@@ -0,0 +1,16 @@
+
+const d = new Date();
+let newDate = d.getTime(); // 1970/1/1 theke kotu milisecond span korlo, ta ber kore
+
+// CURRENT TIME
+let newYear = d.getFullYear();
+let newMonth = d.getMonth();
+let newDate1 = d.getDate();
+let newhours = d.getHours();
+let newMinutes = d.getMinutes();
+let newSecond = d.getSeconds();
+
+
+// MILI SECOND
+let newMiliSecond = d.getMilliseconds();
+console.log(newMiliSecond);
