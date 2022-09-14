2ac7c37747bf9837ed476f52b45a2cf800c8cf41
kusumakter959 <kusumaker.dev@gmail.com>
2022-09-14 09:09:11 +0600

    JavaScript  forLoop

---
+++ js/forLoop.js
@@ -0,0 +1,8 @@
+const cars = ["A", "B", "C", "D", "E"];
+let length = cars.length;
+// console.log(length);
+for(let i = 0; i<length; i++){
+    console.log(i);
+}
+// Variable hocche global scop,
+// let hocche block scope ba function scop
