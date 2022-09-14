ceff50cd1b0245196ad187b2eaf4451c65ea2fb1
kusumakter959 <kusumaker.dev@gmail.com>
2022-09-14 17:24:18 +0600

    JavaScript type Conversion

---
+++ js/typeConversion.js
@@ -0,0 +1,19 @@
+let y = "ksusum";
+let x = +y;
+console.log(typeof x);
+
+// String Convert
+console.log(typeof String(23 + 10));
+console.log(typeof String(210));
+
+// Number Convert
+let d = new Date();
+console.log(typeof Number(d.getTime()));
+
+// Automatic Type Conversion
+
+console.log(5 + null);
+console.log("5" + 2);
+console.log("5" - 2);
+console.log("5" * "2");
+
