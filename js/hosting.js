3a7dc78f941c24b5d9bf48e1d7150bcd654ca97b
kusumakter959 <kusumaker.dev@gmail.com>
2022-09-15 10:41:19 +0600

    JavaScript Hosting

---
+++ js/hosting.js
@@ -0,0 +1,15 @@
+// var age somporno code k pore.. then declier k opore niye jai,, okhane vluye hisabe undefind set kore
+x = 5;
+console.log(x);
+var x;
+
+// Let undefined set kore na ei jonno error dekhai
+x1 = 5;
+console.log(x1); //kichu na dile tokhon var hisabe dore ney
+// let x1;
+
+x2 = 5;
+console.log(x2,y2); //value niche dile undefined hoy
+y2 = 7;
+var x;
+var y;
