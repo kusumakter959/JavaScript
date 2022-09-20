dc493a282dbf51089c7111ad8e2b66929038029a
kusumakter959 <kusumaker.dev@gmail.com>
2022-09-20 09:09:58 +0600

    JavaScript HTML DOM

---
+++ DOM/main.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById("demo").innerHTML = "hello Wold";
+console.log(document.getElementsByTagName("a"));
+
+console.log(document.querySelector("a"));
+console.log(document.querySelectorAll("a"));
+
+
