8aeeb7f7d9b3b1224cb696763aa38199ae47c483
kusumakter959 <kusumaker.dev@gmail.com>
2022-09-20 16:17:20 +0600

    JavaScript Events

---
+++ DOM/event/main.js
@@ -0,0 +1,11 @@
+
+
+function changeText(id){
+    // const txt = document.getElementById("demo");
+    id.innerHTML = "hello BANGLADESH";
+}
+
+const button = document.getElementById("btn");
+button.onclick = function(){
+    console.log("hello world");
+}
