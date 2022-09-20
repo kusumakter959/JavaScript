4c7690cf9bd59e1c84cd55373c0c68ec2f42c432
kusumakter959 <kusumaker.dev@gmail.com>
2022-09-20 17:59:28 +0600

    JavaScript   DOM Navigation

---
+++ DOM/navigation/main.js
@@ -0,0 +1,10 @@
+
+
+const p = document.getElementById("demo");
+const p2 = document.getElementById("demo2");
+// console.log(p.innerHTML);
+const way1 = p.firstChild.nodeValue
+const way2 = p.childNodes[0]
+
+// document.getElementById("demo").innerHTML = p2.innerHTML
+// console.log(document.getElementById("demo").innerHTML = p2.innerHTML);
