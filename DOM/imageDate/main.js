8b6fd933404923ffe2bd8e15b2e5d560a062b6ff
kusumakter959 <kusumaker.dev@gmail.com>
2022-09-20 10:23:20 +0600

    JavaScript DOM HTML

---
+++ DOM/imageDate/main.js
@@ -0,0 +1,10 @@
+
+const myImage = document.getElementById('semiler');
+myImage.src = "test.jpg";
+// console.dir(myImage);
+
+
+// DATE
+const element = document.getElementById('demo').innerHTML = `tody's date ${Date()}`;
+// element.innerHTML = Date();
+// console.dir(date);
