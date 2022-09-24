1a350b3c4d3992373e157576526c7618cdffc6ea
kusumakter959 <kusumaker.dev@gmail.com>
2022-09-24 20:39:34 +0600

    JaveScript History API

---
+++ API/history-API/main.js
@@ -0,0 +1,15 @@
+
+
+function back(){
+    window.history.back();
+}
+function back(n){
+    window.history.go(n); //koyta kore back jabe ba samne jabe
+}
+function forword(){
+    window.history.forward();
+}
+function getHistory(){
+    alert(window.history.length);
+}
+
